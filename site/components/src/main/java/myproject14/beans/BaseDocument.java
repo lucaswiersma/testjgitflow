8cb09c1a62e4734e181f75fdb099dccbd1143fd0
Lucas Wiersma <lucas.wiersma@alvant.nl>
2022-05-30 10:09:45 +0200

    first commit

---
+++ site/components/src/main/java/myproject14/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package myproject14.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="myproject14:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
